827cdddca916415dc8b2e4548d9f8e6852397bc7
skzzbom4545 <meloyansveta399@gmail.com>
2025-01-17 01:54:17 +0400

    Add files via upload

---
+++ 16.01.25-JS/app.js
@@ -0,0 +1,20 @@
+
+var a = "xndrum em yntrel nshvac apranqneric meky 1.xndzor,2.tandz,3.mandarin"
+prompt(a)
+var xkg = 500;
+var kg = prompt("xndrum em nsheq te qani kg e harkavor");
+var vchar = xkg * kg;
+document.write ("duq petq e vjareq" + vchar + " dram");
+
+
+
+// var a = prompt()
+// if (a == 5) {
+//     alert("okay" + 5)
+// }
+// if (a == 6) {
+//     alert("okay" + 6)
+// }
+// if (a == 0) {
+//     alert("no")
+// }
